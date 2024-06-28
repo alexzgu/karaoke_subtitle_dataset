18b31f2f52da33bcc6059737b38c760f8c8e7590
Alexander Gu <alexanderzgu@gmail.com>
2024-06-28 01:47:56 -0400

    refactoring

--- data_processing/stage_2_processing.py
+++ data_processing/stage_2_processing.py
@@ -2,11 +2,13 @@ import os
 import pandas as pd
 
 
+# helper
 def unformatted(text: str):
     # remove any '|'
     return text.replace('|', '')
 
 
+# helper
 def chain_labeling(df: pd.DataFrame):
     # adds a column containing unformatted text
     df['unformatted'] = df['text'].apply(unformatted)
@@ -22,7 +24,8 @@ def chain_labeling(df: pd.DataFrame):
     return df
 
 
-def print_singletons(df, filename, output_df):
+# adds columns to the dataframe to help identify singletons
+def find_singletons(df, filename, output_df):
     # adds a column containing the name of the file
     df['file_name'] = filename
 
@@ -37,14 +40,33 @@ def print_singletons(df, filename, output_df):
     # true if this row and the next row both have 'True' in the 'start_of_new_chain' column
     df['singleton'] = df['start_of_new_chain'] & df['start_of_new_chain'].shift(-1)
 
+    return df
+
+
+# removes rows with null values or empty unformatted values
+def edge_cases(df, filename):
+    # if there are any nulls, print
+    if df.isnull().values.any():
+        print(filename + " has null values")
+        df = df.dropna(how='any', axis=0)
+
+    # if a row has unformatted = "", print into console and remove
+    # just for you, 91.csv ;)
+    if (df['unformatted'] == "").any():
+        print(filename + " has empty unformatted values")
+        df = df[df['unformatted'] != ""]
+
+    return df
+
+
+# prints rows to output_df if singleton is true
+def print_singletons(df, output_df):
     # adds rows to output_df if singleton is true
     for i, row in df.iterrows():
         if row['singleton']:
             output_df.loc[len(output_df)] = [row['start'], row['end'], row['position'], row['line'], row['text'],
                                              row['file_name'], row['unformatted'],
                                              row['start_of_new_chain'], row['singleton']]
-
-    return df
 
 
 def main():
@@ -56,18 +78,11 @@ def main():
         with open(os.path.abspath("../data/stage_1_processed/" + filename)) as f:
             df = pd.read_csv(f)
 
-            df = print_singletons(df, filename, output_df)
+            df = find_singletons(df, filename, output_df)
 
-            # if there are any nulls, print
-            if df.isnull().values.any():
-                print(filename + " has null values")
-                df = df.dropna(how='any', axis=0)
+            df = edge_cases(df, filename)
 
-            # if a row has unformatted = "", print into console and remove
-            # just for you, 91.csv ;)
-            if (df['unformatted'] == "").any():
-                print(filename + " has empty unformatted values")
-                df = df[df['unformatted'] != ""]
+            print_singletons(df, output_df)
 
             df.to_csv(os.path.abspath("../data/stage_2_processed/csvs/" + filename), index=False)
 
